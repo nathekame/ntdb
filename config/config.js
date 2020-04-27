02f2b801a3fb403b2d4857845ee8ab8beaaa0b31
n.athekame <n.athekame@gmail.com>
2020-04-27 21:59:24 +0100

    added api

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 
 
 const config = {
-    apiUrl : "http://localhost:5000/",
+  //  apiUrl : "http://localhost:5000/",
+   apiUrl:  'https://ntdbapi.herokuapp.com/'
    // testEnv : "this is a test string"
 }
 
