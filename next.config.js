de989d6bf7d06ac68d84eb2db0c3934d849937c8
n.athekame <n.athekame@gmail.com>
2020-05-01 05:44:42 +0100

    webpack edit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require("@zeit/next-css");
 const path = require("path");
+
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin')
+
+
 module.exports = withCSS(withSass({
    webpack (config, options) {
        config.module.rules.push({
@@ -13,8 +17,15 @@ module.exports = withCSS(withSass({
            }
        });
 
+       config.plugins.push(
+        new FilterWarningsPlugin({
+            exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
+        }),
+    )
+
        config.resolve.modules.push(path.resolve("./"));
        return config;
    }
 }));
+
 
